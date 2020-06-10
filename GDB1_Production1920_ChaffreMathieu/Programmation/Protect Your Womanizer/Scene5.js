d201ac6ac0f7617b5498a6a7e86e375bf5a40576
Mathieu-chaffre <chaffrem@gmail.com>
2020-06-10 15:58:30 +0200

    update

--- GDB1_Production1920_ChaffreMathieu/Programmation/Protect Your Womanizer/Scene5.js
+++ GDB1_Production1920_ChaffreMathieu/Programmation/Protect Your Womanizer/Scene5.js
@@ -9,11 +9,34 @@ class Scene5 extends Phaser.Scene {
 
   preload(){
     this.load.image("landscape_dance", "assets/landscape_dance.png");
+    this.load.image("mask", "assets/mask.png");
   }
 
   create(){
 
-    this.land_scape = this.add.image(0,0, "landscape_dance").setOrigin(0,0);
+    this.landscape = this.add.image(0,0, "landscape_dance").setOrigin(0,0);
+
+    this.test = this.make.sprite({
+        x: 400,
+        y: 300,
+        key: 'mask',
+        add: false
+    });
+
+    this.test.setInteractive();
+
+
+    this.landscape.mask = new Phaser.Display.Masks.BitmapMask(this, this.test);
+
+
+    this.input.on("pointermove", function(pointer, gameObjects){
+
+        gameObjects[0].x = pointer.x;
+        gameObjects[0].y = pointer.y;
+
+
+
+    });
 
   }
 
